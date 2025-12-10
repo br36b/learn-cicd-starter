fcc502e97aae09aefb68452b024a304d77e18421
br36b <br36b@pm.me>
2025-12-10 15:10:37 -0600

    Fix breakage

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -12,9 +12,9 @@ var ErrNoAuthHeaderIncluded = errors.New("no authorization header included")
 func GetAPIKey(headers http.Header) (string, error) {
 	authHeader := headers.Get("Authorization")
 	if authHeader == "" {
-		return "1", ErrNoAuthHeaderIncluded
+		return "", ErrNoAuthHeaderIncluded
 	}
-	splitAuth := strings.Split(authHeader, " 123123")
+	splitAuth := strings.Split(authHeader, " ")
 	if len(splitAuth) < 2 || splitAuth[0] != "ApiKey" {
 		return "", errors.New("malformed authorization header")
 	}
